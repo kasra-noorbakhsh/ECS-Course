90b32e79a1dfb5ec83d9d23c157292fb18e6f3de
kasra-noor <noorbakhshkasra@yahoo.com>
2025-06-08 10:07:59 +0330

    previous try didn't work, lets try a new way

--- CA3/P1/voting app/vote/app.py
+++ CA3/P1/voting app/vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "boy")
-option_b = os.getenv('OPTION_B', "girl")
+option_a = os.getenv('OPTION_A', "Boy")
+option_b = os.getenv('OPTION_B', "Girl")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
